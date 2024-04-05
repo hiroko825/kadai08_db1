73d95b1612842c936deef8823dd4f211977df455
hiroko825 <159022886+hiroko825@users.noreply.github.com>
2024-04-05 21:51:53 +0900

    Delete register directory

--- register/sql.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE makeup_artists (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(64) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    address VARCHAR(50) NOT NULL,
-    experience ENUM('あり', 'なし') NOT NULL
-);
